a9a7a594c7ac7825d9d0e45afa22d9e4ad262865
Dave Syer <dsyer@pivotal.io>
2014-08-29 13:34:37 +0100

    Update package names

--- src/main/java/locksdemo/LocksApplication.java
+++ src/main/java/locksdemo/LocksApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.platform.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @ComponentScan
